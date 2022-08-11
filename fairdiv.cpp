c904c697dc777a2b2442e9f8caac8bb5b39b4a6d
Shifatur-Rahman <59673387+Shifatur-Rahman@users.noreply.github.com>
2022-08-11 18:11:26 +0600

    Add files via upload

---
+++ fairdiv.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    long long int t,n,a;
+    cin>>t;
+    while(t--){
+        int div = 0, sum = 0,one = 0;
+        cin>>n;
+        for(int i=0; i<n; i++){
+            cin>>a;
+            if(a == 1){
+                one++;
+                sum++;
+            }
+            else
+                sum +=2;
+        }
+        if(sum % 2 == 0){
+            div = sum/2;
+            if(div % 2 == 0){
+                cout<<"YES"<<endl;
+            }
+            else{
+                if(one){
+                    cout<<"YES"<<endl;
+                }
+                else{
+                    cout<<"NO"<<endl;
+                }
+            }
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+
+    }
+    return 0;
+
+
+}
